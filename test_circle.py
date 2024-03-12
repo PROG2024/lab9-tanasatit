fd6f7b97c6f6ba69abe4568ad3e38fadbca89b10
tanasatit <tanasatit.n@ku.th>
2024-03-13 02:55:08 +0700

    commit 1

--- test_circle.py
+++ test_circle.py
@@ -9,10 +9,6 @@ Write unit tests as described in README.md.
 """
 
 import math
-
-from circle import Circle
-
-# TODO write 3 tests as described above
 import unittest
 from circle import Circle
 
@@ -24,20 +20,19 @@ class CircleTest(unittest.TestCase):
         self.circle2 = Circle(2)
         self.circle3 = Circle(0)
 
-        self.r1 = self.circle1.radius
-        self.r2 = self.circle2.radius
-        self.r3 = self.circle3.radius
+        self.r1 = self.circle1.get_radius()
+        self.r2 = self.circle2.get_radius()
+        self.r3 = self.circle3.get_radius()
 
     def test_add_area(self):
-        self.assertEqual(self.circle1.add_area(self.circle2).radius, Circle(math.sqrt(self.r1**2+self.r2**2)).radius)
-        self.assertEqual(self.circle1.add_area(self.circle2).get_area(), Circle(math.sqrt(self.r1**2+self.r2**2)).get_area())
+        self.assertEqual(self.circle1.add_area(self.circle2).get_radius(),
+                         Circle(math.sqrt(self.r1**2+self.r2**2)).get_radius())
+        self.assertEqual(self.circle1.add_area(self.circle2).get_area(),
+                         Circle(math.sqrt(self.r1**2+self.r2**2)).get_area())
 
     def test_edge_case(self):
-        self.assertEqual(self.circle1.add_area(self.circle3).radius, Circle(self.r1).radius)
-        self.assertEqual(self.circle3.add_area(self.circle2).radius, Circle(self.r2).radius)
-
+        self.assertEqual(self.circle1.add_area(self.circle3).get_radius(), Circle(self.r1).get_radius())
         self.assertEqual(self.circle1.add_area(self.circle3).get_area(), Circle(self.r1).get_area())
-        self.assertEqual(self.circle3.add_area(self.circle2).get_area(), Circle(self.r2).get_area())
 
     def test_negative_radius(self):
         with self.assertRaises(ValueError):
